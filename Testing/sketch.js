9152e3d0852d3ee2a6d98c7dd29120d17e5ddac8
sooruva <soorria.ss@gmail.com>
2018-03-04 09:14:35 +1100

    Add Directory for testing purposes

---
+++ Testing/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight, P2D);
+}
+
+function draw() {
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function createInfoWindow(position, textObj, ) {
+
+}
+
+class infoWindow {
+
+}
